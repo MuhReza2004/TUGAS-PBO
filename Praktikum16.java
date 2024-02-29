1aa98120bd217158e799f4e9abda5383ad245e38
MuhReza2004 <muh.reza2441@gmail.com>
2024-03-01 01:13:34 +0800

    Update

--- Praktikum16.java
+++ Praktikum16.java
@@ -5,46 +5,77 @@
 public class Praktikum16 {
     public static void main(String[] args) {
 
-        boolean Bool1, Bool2, TF;
-        int i, j, hsl;
-        float x, y, res;
+        boolean Bool1, Bool2, TF, res;
+        int i = 5, j = 2, hsl;
+        float x = 5, y = 5;
 
         System.out.println(
                 "Silahkan baca teksnya dan tambahkan perintah program di bawah ini untuk menampilkan output program");
         Bool1 = true;
         Bool2 = false;
-        TF = Bool1 && Bool2;
-        TF = Bool1 || Bool2;
-        TF = !Bool1;
-        TF = Bool1 ^ Bool2;
+        // Operasi boolean
+        TF = Bool1 && Bool2; // Boolean AND
+        System.out.println("Bool1 && Bool2 = " + TF);
 
-        i = 5;
-        j = 2;
-        hsl = i + j;
-        hsl = i - j;
-        hsl = i / j;
-        hsl = i * j;
-        hsl = i / j;
-        hsl = i % j;
+        TF = Bool1 || Bool2; // Boolean OR
+        System.out.println("Bool1 || Bool2 = " + TF);
 
-        x = 5;
-        y = 5;
-        res = x + y;
-        res = x - y;
-        res = x / y;
-        res = x * y;
+        TF = !Bool1; // NOT
+        System.out.println("!Bool1 = " + TF);
 
-        TF = (i == j);
-        TF = (i != j);
-        TF = (i < j);
-        TF = (i > j);
-        TF = (i <= j);
-        TF = (i >= j);
+        TF = Bool1 ^ Bool2; // XOR
+        System.out.println("Bool1 ^ Bool2 = " + TF);
 
-        TF = (x != y);
-        TF = (x < y);
-        TF = (x > y);
-        TF = (x <= y);
-        TF = (x >= y);
+        hsl = i + j; // Penjumlahan
+        System.out.println("i + j = " + hsl);
+
+        hsl = i - j; // Pengurangan
+        System.out.println("i - j = " + hsl);
+
+        hsl = i / j; // Pembagian
+        System.out.println("i / j = " + hsl);
+
+        hsl = i * j; // Perkalian
+        System.out.println("i * j = " + hsl);
+
+        hsl = i / j; // Pembagian bulat
+        System.out.println("i / j (pembagian bulat) = " + hsl);
+
+        hsl = i % j; // Sisa modulo
+        System.out.println("i % j (sisa modulo) = " + hsl);
+
+        res = (i == j); // Sama dengan
+        System.out.println("i == j : " + res);
+
+        res = (i != j); // Tidak sama dengan
+        System.out.println("i != j : " + res);
+
+        res = (i < j); // Kurang dari
+        System.out.println("i < j : " + res);
+
+        res = (i > j); // Lebih dari
+        System.out.println("i > j : " + res);
+
+        res = (i <= j); // Kurang dari atau sama dengan
+        System.out.println("i <= j : " + res);
+
+        res = (i >= j); // Lebih dari atau sama dengan
+        System.out.println("i >= j : " + res);
+
+        // Operasi relasional numerik
+        res = (x != y); // Tidak sama dengan
+        System.out.println("x != y : " + res);
+
+        res = (x < y); // Kurang dari
+        System.out.println("x < y : " + res);
+
+        res = (x > y); // Lebih dari
+        System.out.println("x > y : " + res);
+
+        res = (x <= y); // Kurang dari atau sama dengan
+        System.out.println("x <= y : " + res);
+
+        res = (x >= y); // Lebih dari atau sama dengan
+        System.out.println("x >= y : " + res);
     }
 }
